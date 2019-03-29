1733f7d68325c072c78950ba5dce394f27293dc5
Pruthvi Patel <30744464+pruthvi0728@users.noreply.github.com>
2019-03-30 02:15:01 +0530

    all

--- dbconn.php
+++ dbconn.php
@@ -6,7 +6,7 @@
  * Time: 12:43 AM
  */
 
-$conn = mysqli_connect("localhost","id7335643_root","rajsolanki","id7335643_cust_info");
+$conn = mysqli_connect("localhost","root","","cust_info");
 if(!$conn){
     echo "Database Connection Fail";
 }
