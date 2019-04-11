f8bbb4ec88968a6143158f261d0087575e4d1e6f
Raj Solanki <solankiraj10897@gmail.com>
2019-04-11 17:50:12 +0530

    final

--- dbconn.php
+++ dbconn.php
@@ -6,8 +6,8 @@
  * Time: 12:43 AM
  */
 
-$conn = mysqli_connect("localhost","root","","cust_info");
-$conn_device = mysqli_connect("localhost","root","","device_info");
+$conn = mysqli_connect("localhost","root","root","cust_info");
+$conn_device = mysqli_connect("localhost","root","root","device_info");
 if(!$conn){
     echo "Customer Database Connection Fail";
 }
