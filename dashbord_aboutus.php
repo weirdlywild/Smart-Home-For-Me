156a907310e8776d2cfa2395ac11d8064e102ab1
Raj Solanki <solankiraj10897@gmail.com>
2019-04-21 16:55:22 +0530

    sfedfgf

--- dashbord_aboutus.php
+++ dashbord_aboutus.php
@@ -7,12 +7,11 @@ if (empty($_SESSION['email'])){
 ?>
 <html>
 <head>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+<title>ABOUT US</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
 <link href="https://fonts.googleapis.com/css?family=Karla|Yeseva+One" rel="stylesheet">
-
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel = "icon" href ="images/logo_title.png" type = "image/x-icon">
 	<style>
 		body {
   height: 100%;
